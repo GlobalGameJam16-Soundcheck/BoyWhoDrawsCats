cf1c31a31db5dfca82855d9fe29e431f33877f3a
Akiva42 <akiva@cmu.edu>
2016-03-28 22:41:49 -0400

    project setup, tile system, basic player movment, and level loader.

---
+++ BWDC/Assets/scripts/GridControl.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GridControl : MonoBehaviour {
+
+    int tilesWidth;
+    int tilesHeight;
+    string[,] tiles;
+    public GameObject tile;
+    public TextAsset levelCsv;
+
+    // Use this for initialization
+    void Start () {
+
+       // TextAsset levelCsv = (TextAsset)Resources.Load("level", typeof(TextAsset));
+
+        string[,] tempTiles = CSVReader.SplitCsvGrid(levelCsv.text);
+        tilesWidth = tempTiles.GetLength(0)-2;
+        tilesHeight = tempTiles.GetLength(1)-2;
+        tiles = new string[tilesWidth, tilesHeight];
+        for (int x = 0; x < tilesWidth; x++)
+        {
+            for(int y = 0; y < tilesHeight; y++)
+            {
+                tiles[x, y] = tempTiles[x, tilesHeight-1 - y];
+                if (tiles[x,y] == "B")
+                {
+                   Instantiate(tile, new Vector3(x, y, 0), Quaternion.identity);
+                }
+            }
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ BWDC/Assets/scripts/movment.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class movment : MonoBehaviour {
+    public GameObject heighlight;
+    Vector3 dest;
+
+	// Use this for initialization
+	void Start () {
+        dest = transform.position;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonUp(0))
+        {
+            dest = new Vector3 (Mathf.Round(Camera.main.ScreenToWorldPoint(Input.mousePosition).x), Mathf.Round(Camera.main.ScreenToWorldPoint(Input.mousePosition).y), 0);
+            heighlight.transform.position = dest;
+        }
+        transform.position = Vector3.MoveTowards(transform.position, new Vector3(dest.x, transform.position.y, 0), 0.2f);
+    }
+}
